5ba22d0bc20598488d17896b333df9e1804faca6
Aidan647 <kirillkotka@gmail.com>
2024-03-23 01:14:16 +0200

    map > forEach

--- src/snippets/snippets.ts
+++ src/snippets/snippets.ts
@@ -1,21 +1,15 @@
 import { snippetCompletion, type Completion } from "@codemirror/autocomplete"
 import allInstructions from "./allInstructions"
 
-const snippets: Completion[] = []
-
-export default snippets
-
-
-
-allInstructions.forEach((instruction) => {
+export const snippets: Completion[] = allInstructions.map((instruction) => {
 	const snip = instruction.snippet.length === 0 ? instruction.name : `${instruction.name} ${instruction.snippet}`
-	snippets.push(
-		snippetCompletion(snip, {
+	return snippetCompletion(snip, {
 			boost: instruction.deprecated !== false ? -99 : -10,
 			label: instruction.name,
 			detail: instruction.preview.slice(instruction.name.length + 1),
 			info: instruction.description,
 			type: "function",
-		}),
-	)
+		})
 })
+
+export default snippets
