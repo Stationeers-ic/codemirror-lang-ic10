e3f64d32903b8dfbc52c995f5be65a1117bf683a
Aidan647 <kirillkotka@gmail.com>
2024-05-15 17:21:09 +0300

    1

--- tools/update_data.ts
+++ tools/update_data.ts
@@ -1,25 +1,43 @@
-import fs from 'fs/promises';
-import { dirname, join } from 'path';
-let grammar = await fs.readFile(join(dirname(__dirname),'src','syntax.grammar'), 'utf8')
+import fs from "fs/promises"
+import { dirname, join } from "path"
+let grammar = await fs.readFile(join(dirname(__dirname), "src", "syntax.grammar"), "utf8")
 
 // read src/syntax.grammar
 {
-const consts = await(await fetch('https://assets.ic10.dev/consts.json')).json()
-const regex = /\/\/ startConst(.*)\/\/ endConst/gs;
-grammar = grammar.replace(regex, `// startConst\n\t\t${Object.keys(consts).sort().join(" |\n\t\t")}\n// endConst`)
+	const consts = await fetch("https://assets.ic10.dev/consts.json").then((e) => e.json())
+	const regex = /\/\/ startConst(.*)\/\/ endConst/gs
+	grammar = grammar.replace(
+		regex,
+		`// startConst\n\t\t${Object.keys(consts)
+			.sort()
+			.map((x) => `"${x}"`)
+			.join(" |\n\t\t")}\n\t\t// endConst`,
+	)
 }
 {
-	const logics = await(await fetch('https://assets.ic10.dev/languages/EN/logics.json')).json()
-	const regex = /\/\/ startLogic(.*)\/\/ endLogic/gs;
-	grammar = grammar.replace(regex, `// startLogic\n\t\t${logics.data.filter((e:any)=>e.name).map((e:any)=>e.name).sort().join(" |\n\t\t")}\n// endLogic`)
+	const logics = await fetch("https://assets.ic10.dev/languages/EN/logics.json").then((e) => e.json())
+	const regex = /\/\/ startLogic(.*)\/\/ endLogic/gs
+	grammar = grammar.replace(
+		regex,
+		`// startLogic\n\t\t${logics.data
+			.filter((e: any) => e.name)
+			.map(
+				(e: any) => `"${e.name}"`)
+			.sort()
+			.join(" |\n\t\t")}\n\t\t// endLogic`,
+	)
 }
 {
-	const instructions = await(await fetch('https://assets.ic10.dev/languages/EN/instructions.json')).json()
-	const regex = /\/\/ startInstruction(.*)\/\/ endInstruction/gs;
-	grammar = grammar.replace(regex, `// startInstruction\n\t\t${Object.keys(instructions).sort().join(" |\n\t\t")}\n// endInstruction`)
-
+	const instructions = await fetch("https://assets.ic10.dev/languages/EN/instructions.json").then((e) => e.json())
+	const regex = /\/\/ startInstruction(.*)\/\/ endInstruction/gs
+	grammar = grammar.replace(
+		regex,
+		`// startInstruction\n\t\t${Object.keys(instructions)
+			.sort()
+			.map((x) => `"${x}"`)
+			.join(" |\n\t\t")}\n\t\t// endInstruction`,
+	)
 }
-fs.writeFile(join(dirname(__dirname),'src','syntax.grammar'), grammar)
+fs.writeFile(join(dirname(__dirname), "src", "syntax.grammar"), grammar)
 
-export { };
-
+export {}
